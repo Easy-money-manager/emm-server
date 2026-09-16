19ffc239f3b78193060bf88205f4612d75522361
Evrandil <grzes.zapala@gmail.com>
2026-09-16 09:52:44 +0200

    Seems like we have everything done here for now, we'll see in tests if it works

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ use crate::server::Server;
 #[tokio::main]
 async fn main() {
     let database = Database::new("easy_money_manager.db").expect("Failed to initialize database");
-    match database.initialize() {
-        Ok(())     => eprintln!("Initialized database"),
-        Err(error) => eprintln!("Failed to initialize database: {}", error),
-    };
+    database.initialize().expect("Failed to initialize database");
     let server = Server::new(database);
     if let Err(error) = server.run().await {
         eprintln!("Server failed: {}", error);
