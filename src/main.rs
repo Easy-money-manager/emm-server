9a0b8a1c06d3bc4bab07b82a15ddaf99dddb0fe8
Evrandil <grzes.zapala@gmail.com>
2026-09-16 12:15:12 +0000

    Ayo

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use crate::server::Server;
 
 #[tokio::main]
 async fn main() {
-    let database = Database::new("easy_money_manager.db").expect("Failed to initialize database");
+    let database = Database::new("/var/lib/emm/easy_money_manager.db").expect("Failed to initialize database");
     database.initialize().expect("Failed to initialize database");
     let server = Server::new(database);
     if let Err(error) = server.run().await {
