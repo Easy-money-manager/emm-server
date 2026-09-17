2a359048b1f7446d2a7687622ad149b48e7caf95
Evrandil <grzes.zapala@gmail.com>
2026-09-17 13:05:03 +0200

    Just a bit of indentation

--- src/server.rs
+++ src/server.rs
@@ -331,10 +331,10 @@ impl Server {
     }
 
     pub async fn run(self) -> Result<(), Box<dyn std::error::Error>> {
-    let app = self.router();
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
-    axum::serve(listener, app).await?;
-    Ok(())
-}
+        let app = self.router();
+        let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+        axum::serve(listener, app).await?;
+        Ok(())
+    }
 }
 
